ac4a26b905d0ebd01de6ae1362f3f0f78dae95dd
Jimputin <89787903+Jimputinfn@users.noreply.github.com>
2024-02-03 13:53:28 +0200

    Create AndroidDebloaderWin.cmd

---
+++ AndroidDebloaderWin.cmd
@@ -0,0 +1,11 @@
+echo off 
+title Android Debloader | https://github.com/Jimputinfn/AndroidDebloader
+color a
+cls
+
+echo you have to have ADB installed.
+pause
+echo Do you see your phone on this list?
+adb devices
+Echo if you do press enter or any key
+pause
